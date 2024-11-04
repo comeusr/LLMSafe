db2b93d022f48bf7cd8c711d28d5f02a9df6b047
Rajdeep Haldar <104543504+rhaldarpurdue@users.noreply.github.com>
2024-11-04 14:27:17 -0500

    Update trainers.py

--- trainers.py
+++ trainers.py
@@ -1436,10 +1436,12 @@ class KLTrainer(UnpairedPreferenceTrainer):
         chosen_logratios = (policy_chosen_logps - reference_chosen_logps)
         rejected_logratios = (policy_rejected_logps-reference_rejected_logps)
 
+        EPS = 1e-6
+
         rejected_ratios_power = torch.exp(self.config.loss.beta*rejected_logratios) #torch.pow(torch.exp(rejected_logratios), self.config.loss.beta)
         self.moving_avg_track = (1-self.moving_avg_rate)*self.moving_avg_track+self.moving_avg_rate*rejected_ratios_power.mean().detach()
         
-        losses = torch.cat(-self.config.loss.beta*chosen_logratios, 1/self.moving_avg_track*rejected_ratios_power)
+        losses = torch.cat(-self.config.loss.beta*chosen_logratios, 1/(self.moving_avg_track+EPS)*rejected_ratios_power)
         
         chosen_rewards = self.config.loss.beta*chosen_logratios.detach()
         rejected_rewards = self.config.loss.beta*rejected_logratios.detach() #1/self.moving_avg_track*rejected_ratios_power.detach()
